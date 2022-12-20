b4f3adf80a18af0841e615149814c16c958a2611
eduardosdl <97840495+eduardosdl@users.noreply.github.com>
2022-12-19 22:27:13 -0300

    edit database pass and fix database connection

--- pipeline/app/conexao.php
+++ pipeline/app/conexao.php
@@ -1,18 +1,8 @@
 <?php
-$servername = "";
-$username = "root";
-$password = "Senha123";
-$database = "meubanco";
+$link = new mysqli("connection-mysql", "root", "senha123", "meubanco");
 
-// Criar conexão
-
-
-$link = new mysqli($servername, $username, $password, $database);
-
-/* check connection */
 if (mysqli_connect_errno()) {
     printf("Connect failed: %s\n", mysqli_connect_error());
     exit();
 }
-
 ?>
